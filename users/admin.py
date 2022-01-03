58a4ae15f496c1cee064eefb06ff3c234e613ce6
Ghazaal <ghazal_edrisabadi@comp.iust.ac.ir>
2022-01-04 02:01:56 +0330

    Register staff user models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Staff_NID, Staff_SID, Staff_tel, Technician_staff, Administrative_staff
 # Register your models here.
+admin.site.register(Staff_NID)
+admin.site.register(Staff_SID)
+admin.site.register(Staff_tel)
+admin.site.register(Technician_staff)
+admin.site.register(Administrative_staff)
