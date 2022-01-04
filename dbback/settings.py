15fb27fc61d3472c188e8a58546692416029389b
amin1998sh <amin7799sh@gmail.com>
2022-01-04 14:58:32 +0330

    Create violations app.

--- dbback/settings.py
+++ dbback/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'maintenances', #An app that me(Amin) created.
     'cameras', #An app that me(Amin) created.
     'headquarters', #An app that me(Amin) created.
+    'violations', #An app that me(Amin) created.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
