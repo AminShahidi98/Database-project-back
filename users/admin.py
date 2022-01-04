f619eb8bc79265133931c165174a5f9fd1889318
Ghazaal <ghazal_edrisabadi@comp.iust.ac.ir>
2022-01-04 23:13:02 +0330

    Add models to admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Staff_NID, Staff_SID, Staff_tel, Technician_staff, Administrative_staff
+from .models import *
 # Register your models here.
 admin.site.register(Staff_NID)
 admin.site.register(Staff_SID)
 admin.site.register(Staff_tel)
 admin.site.register(Technician_staff)
 admin.site.register(Administrative_staff)
+admin.site.register(Car_owner)
+admin.site.register(Ownership)
